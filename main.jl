238bbd91f5ec6b5fe8f5273a0fbd2fb2c281d6e7
Paul Pukite <paul@pukite.com>
2026-02-26 21:29:41 -0600

    Fix soft scope variable warnings in main.jl

--- main.jl
+++ main.jl
@@ -15,33 +15,37 @@ end
 
 # Check if file has a header by test-reading (handles whitespace/comma delimiting and strips whitespace)
 test_df = CSV.read(data_path, DataFrame; limit=1, delim=' ', ignorerepeated=true, stripwhitespace=true)
+
+times = Float64[]
+data = Float64[]
+
 if "year_fraction" in names(test_df) && "nino34_normalized" in names(test_df)
-    df = CSV.read(data_path, DataFrame; delim=' ', ignorerepeated=true, stripwhitespace=true)
-    times = Float64.(df.year_fraction)
-    data  = Float64.(df.nino34_normalized)
+    local df = CSV.read(data_path, DataFrame; delim=' ', ignorerepeated=true, stripwhitespace=true)
+    global times = Float64.(df.year_fraction)
+    global data  = Float64.(df.nino34_normalized)
 else
     # Fallback for comma if first check failed due to comma instead of space
     test_df_comma = CSV.read(data_path, DataFrame; limit=1, stripwhitespace=true)
     if "year_fraction" in names(test_df_comma) && "nino34_normalized" in names(test_df_comma)
-        df = CSV.read(data_path, DataFrame; stripwhitespace=true)
-        times = Float64.(df.year_fraction)
-        data  = Float64.(df.nino34_normalized)
+        local df = CSV.read(data_path, DataFrame; stripwhitespace=true)
+        global times = Float64.(df.year_fraction)
+        global data  = Float64.(df.nino34_normalized)
     else
         # Assume 1st column is year, 2nd column is amplitude (headerless)
         # We'll use CSV with auto-detection but force ignorerepeated in case of arbitrary whitespace
         # We default to space, ignorerepeated, stripwhitespace
         try
-            df = CSV.read(data_path, DataFrame; header=false, delim=' ', ignorerepeated=true, stripwhitespace=true)
+            local df = CSV.read(data_path, DataFrame; header=false, delim=' ', ignorerepeated=true, stripwhitespace=true)
             if size(df, 2) < 2
                 throw(ArgumentError("Less than 2 columns found with space delimiter."))
             end
-            times = Float64.(df[:, 1])
-            data  = Float64.(df[:, 2])
+            global times = Float64.(df[:, 1])
+            global data  = Float64.(df[:, 2])
         catch
             # Fallback to comma separation
-            df = CSV.read(data_path, DataFrame; header=false, stripwhitespace=true)
-            times = Float64.(df[:, 1])
-            data  = Float64.(df[:, 2])
+            local df = CSV.read(data_path, DataFrame; header=false, stripwhitespace=true)
+            global times = Float64.(df[:, 1])
+            global data  = Float64.(df[:, 2])
         end
     end
 end
